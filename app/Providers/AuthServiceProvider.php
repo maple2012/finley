a7eb6759184d7d567673592d746dfd12aff7627f
vagrant <vagrant@homestead>
2019-01-16 23:26:01 +0800

    提交

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+		 \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
+		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class  => \App\Policies\UserPolicy::class,
+    ];
+
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
+    }
+}
